0a4ad2cfb86da29ffa192b9b0f217945458a3a24
pwndubey77 <pwndubey77@gmail.com>
2018-11-10 16:41:03 +0530

    added user data

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var user = require('../models/user');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+   router.get('/', function(req, res, next) {
+    user.find({}, function (err, docs) {
+      res.send(JSON.stringify(docs));
+    });
 });
+
+
 
 module.exports = router;
